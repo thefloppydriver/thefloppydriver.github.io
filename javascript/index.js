76324e0dceced16354ff71f31c266944b66f234d
thefloppydriver <42722940+thefloppydriver@users.noreply.github.com>
2019-08-22 10:38:28 -0400

    f

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,11 @@
 document.getElementById('date').innerHTML = new Date().toDateString();
+
+function sendMail() {
+    var link = "mailto:9809257025@vtext.com"
+             + "?cc=fearmypowergoodsir@gmail.com"
+             + "&subject=" + escape("This is my subject")
+             + "&body=" + escape(document.getElementById('myText').value)
+    ;
+
+    window.location.href = link;
+}
