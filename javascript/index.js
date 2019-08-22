ecf934aa243c51cde2967b8e2ed8266cb048a72e
thefloppydriver <42722940+thefloppydriver@users.noreply.github.com>
2019-08-22 12:08:04 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -9,3 +9,13 @@ function sendMail() {
 
     window.location.href = link;
 }
+
+function emailjs() {
+    Email.send({
+    SecureToken : "f783e512-0f19-4c3c-b5e3-b15c5acd1ae3",
+    To : "9809257025@vtext.com",
+    From : "fearmypowergoodsir@gmail.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+    }).then(message => alert(message))
+}
