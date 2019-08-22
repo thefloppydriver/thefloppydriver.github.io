01237fd1c81194a4e7b086adcde249931101943d
thefloppydriver <42722940+thefloppydriver@users.noreply.github.com>
2019-08-22 18:08:40 -0400

    UPDATE PLSSSSS

--- javascript/index.js
+++ javascript/index.js
@@ -4,7 +4,7 @@ function emailjs() {
     return document.getElementById("numberOrContact").value;
     return document.getElementById("victimsCarrier").value;
     alert(document.getElementById("numberOrContact").value);
-    alert(document.getElementById("numberOrContact").value);
+    alert(document.getElementById("victimsCarrier").value);
     Email.send({
     SecureToken : "83dafd07-ceaf-458d-82a9-6ec6313b46f4",
     To : "9809257025@vtext.com",
