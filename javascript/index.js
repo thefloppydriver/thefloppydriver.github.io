84b6925bf8f07fc8aeb2bb9f97fad85484439395
thefloppydriver <42722940+thefloppydriver@users.noreply.github.com>
2019-08-22 18:19:29 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,14 @@
 //document.getElementById('date').innerHTML = new Date().toDateString();
 function emailjs() {
-    
-    return document.getElementById("numberOrContact").value;
-    return document.getElementById("victimsCarrier").value;
+    var numberOrContactVariable = document.getElementById("numberOrContact").value;
     alert(document.getElementById("numberOrContact").value);
     alert(document.getElementById("victimsCarrier").value);
+    
     Email.send({
     SecureToken : "83dafd07-ceaf-458d-82a9-6ec6313b46f4",
     To : "9809257025@vtext.com",
     From : "fearmypowergoodsir@gmail.com",
-    Subject : "(REEEE)",
+    Subject : "REEEE",
     Body : "MESSAGE"
-    }).then(message => alert(message));
+    }).then(message => console.log(message));
 }
