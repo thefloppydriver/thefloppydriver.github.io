7b5242d8656455ac7dea2b8b523dce58a6fa37e0
thefloppydriver <42722940+thefloppydriver@users.noreply.github.com>
2019-08-22 21:20:50 -0400

    here we go

--- javascript/index.js
+++ javascript/index.js
@@ -4,8 +4,6 @@ function emailjs() {
     alert(numberOrContactVariable + " I WANT CAKE");
     alert("PLEASE GIVE ME CAKE");
     alert(document.getElementById("victimsCarrier").value);
-    alert("unghhhhhhnnnnnnnnnn");
-    /*'
     Email.send({
     SecureToken : "83dafd07-ceaf-458d-82a9-6ec6313b46f4",
     To : "9809257025@vtext.com",
@@ -14,4 +12,14 @@ function emailjs() {
     Body : "MESSAGE"
     }).then(message => console.log(message));
     */
+}
+
+function alert_ryan() {
+	Email.send({
+    SecureToken : "83dafd07-ceaf-458d-82a9-6ec6313b46f4",
+    To : "9809257025@vtext.com",
+    From : "fearmypowergoodsir@gmail.com",
+    Subject : "ALERT"+Math.floor((Math.random() * 10000) + 1).toString(),
+    Body : "Someone just tried to spam "+document.getElementById("numberOrContact").value
+    }).then(message => console.log(message));
 }
