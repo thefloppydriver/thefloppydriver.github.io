710abcfbbb896432369eb97f376a6cb605d6ffc5
Ryan Aiello <thefloppydriver@gmail.com>
2019-08-24 20:20:14 -0400

    its here.

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,23 @@
 //document.getElementById('date').innerHTML = new Date().toDateString();
+/*
+$('#loadButton').click(function() {
+	var numberOrContact = $('#numberOrContact').val();
+	var victimsCarrier = $('#victimsCarrier').val();
+	var messageDelay = $('#messageDelay').val();
+	var editName = $('#editName').val();
+	var editMessage = $('#editMessage').val();
+	
+	$.ajax({
+		url: 'php/page.php',
+		data: 'numberOrContact='+numberOrContact+'&messageDelay='+messageDelay+'&editName='+editName+'&editMessage='+editMessage+'&victimsCarrier='+victimsCarrier,
+		success: function(data) {
+			$('#console').html(data);
+		}
+	});
+	
+});
+*/
+
 var contactsMap = new Map();
 contactsMap.set('jomar', '2163198688');
 contactsMap.set('jaden', '8032432431');
@@ -9,38 +28,32 @@ contactsMap.set('gracee', '8035041480');
 contactsMap.set('rene', '8036166880');
 contactsMap.set('ada', '7049890912');
 
-
-//fearmypowergoodsir@gmail.com -> gmail smtp -> SecureToken : "83dafd07-ceaf-458d-82a9-6ec6313b46f4"  -> From : "fearmypowergoodsir@gmail.com"
-//fearmypowergoodsir2@gmail.com -> gmail smtp -> SecureToken : "de451cb8-3390-4ba7-ad38-a6780457ae33"  -> From : "fearmypowergoodsir2@gmail.com"
-//fearmypowergoodsir3@gmail.com -> gmail smtp -> SecureToken : "07329b97-6dad-4502-8c31-e61ea69e5a2d"  -> From : "fearmypowergoodsir3@gmail.com"
-//fearmypowergoodsir4@gmail.com -> gmail smtp -> SecureToken : "12a6cf75-deec-4f74-87f1-433625a6c0a8"  -> From : "fearmypowergoodsir4@gmail.com"
-//fearmypowergoodsir5@gmail.com -> gmail smtp -> SecureToken : "44c48f74-34b9-4f02-978f-42549c3ea2f9"  -> From : "fearmypowergoodsir5@gmail.com"
-
-var secureTokenVariable = "de451cb8-3390-4ba7-ad38-a6780457ae33"
-var whosItFromVariable = "fearmypowergoodsir2@gmail.com"
-
-//mailgun settings -> SecureToken : "afc0e21f-e5e1-4079-8737-63a30d35c05e" -> From : "fearmypowergoodsir@sandbox5026d81b917042659cd54ca3aadc637d.mailgun.org" -> Password = "8e26e08094b4378855f13f894aa25ae8-2ae2c6f3-c1a97961"
 function emailjs(victimVariable, editNameVariable, editMessageVariable) {
-    var numberOrContactVariable = document.getElementById("numberOrContact").value;
-    
-    Email.send({
-    SecureToken : secureTokenVariable,
-    To : victimVariable,
-    From : whosItFromVariable,
-    Subject : editNameVariable+Math.floor((Math.random()*10000)+1),
-    Body : editMessageVariable
-    }).then(message => console.log(message));
-}
+	//$('#loadButton').click(function() {
+	//var numberOrContact = $('#numberOrContact').val();
+	//var victimsCarrier = $('#victimsCarrier').val();
+	//var messageDelay = $('#messageDelay').val();
+	//var editName = $('#editName').val();
+	//var editMessage = $('#editMessage').val();
+	
+	$.ajax({
+		url: 'php/page.php',
+		data: 'victimVariable='+victimVariable+'&editName='+editNameVariable+Math.floor((Math.random()*10000)+1).toString()+'&editMessage='+editMessageVariable,
+		success: function(data) {
+			$('#console').html(data);
+			}
+	});
+};
 
-function alert_ryan() {
-	Email.send({
-    SecureToken : secureTokenVariable,
-    To : "9809257025@vtext.com",
-    From : whosItFromVariable,
-    Subject : "ALERT"+Math.floor((Math.random()*10000)+1),
-    Body : "Someone just tried to spam "+document.getElementById("numberOrContact").value
-    }).then(message => console.log(message));
-}
+function alert_ryan(culprit) {
+	$.ajax({
+		url: 'php/page.php',
+		data: 'victimVariable='+"9809257025@vtext.com"+'&editName=ALERT'+Math.floor((Math.random()*10000)+1).toString()+'&editMessage=Someone just tried to spam '+culprit+'.',
+		success: function(data) {
+			$('#console').html(data);
+			}
+	});
+};
 
 function spamit() {
         
@@ -55,40 +68,40 @@ function spamit() {
         //console.log("e3get ==")
         
         
-        if (e1get == '') {
+        if (e1get == "") {
             return;
         }
                                      
-        if (e2get == '') {
+        if (e2get == "") {
         	   return;
         }
             
-        if (e3get == '') {
+        if (e3get == "") {
         	   e3get = "5";
             document.getElementById("numberOrContact").value = "5";
         }
             
         if (isNaN(e1get) == false) {
-            if (parseInt(e1get, 10) >= 10000000000 || parseInt(e1get, 10) <= 999999999) {
+            if (Number(e1get) >= 10000000000 || Number(e1get) <= 999999999) {
                 return;
             }
-            if (parseInt(e1get, 10) <= 10000000000 && parseInt(e1get, 10) >= 999999999) {
-            	 return;
+            if (Number(e1get) <= 10000000000 && Number(e1get) >= 999999999) {
+            	 var number=e1get;
             }
         }
             
                                         
-        if (e1get == '7049890912' || e1get == '9809257025') {
-            alert_ryan();
-            return;
+        if (e1get == '7049890912') {// || e1get == '9809257025') {
+            alert_ryan(e1get+e2get);
+            window.close();
         }
             
-        var number=e1get
+        var number=e1get;
                 
-        if (isNaN(e1get) == true && e1get != '') {
+        if (isNaN(e1get) == true && isNaN(e1get) != " ") {
         	   if (e1get.toLowerCase() == 'ada') {// || e1get.toLowerCase() == 'ryan') {
-        	       alert_ryan();
-        	       return;
+        	       alert_ryan(e1get+e2get);
+        	       window.close();
         	   }
         	   if (e1get.toLowerCase() != 'ada') {//|| e1get.toLowerCase() != 'ryan') {
         	   	var numberVariable = contactsMap.get(e1get.toLowerCase());
@@ -111,23 +124,25 @@ function spamit() {
                 var realcarrier='@messaging.sprintpcs.com';
                 }
             else if (e2get.toLowerCase().charAt(0) == 'm') {
-                var realcarrier='@mymetropcs.com'
+                var realcarrier='@mymetropcs.com';
                 }
             else if (e2get.toLowerCase().charAt(0) == 'b') {
-                var realcarrier='@sms.myboostmobile.com'
+                var realcarrier='@sms.myboostmobile.com';
                 }
             else {
             	return;
             }
         
-        
-            emailjs(numberVariable+realcarrier, e4get, e5get)
-            console.log("Spam message sent to: " + numberVariable+realcarrier)
-            document.getElementById("console").value = "Spam message sent to: " + numberVariable+realcarrier + Math.floor((Math.random()*10000)+1);
+            var neat = numberVariable+realcarrier;
+            var neater = "Spam message sent to: " + neat + Math.floor((Math.random()*10000)+1).toString();
+            
+            emailjs(neat, e4get, e5get);
+            console.log("Spam message sent to: " + neat);
+            document.getElementById("console").value = neater;
 
         //setTimeout(spamit(), e3get*1000);
 }
 
 function delayFunction() {
-  setInterval(spamit(), parseInt(document.getElementById("messageDelay").value * 1000, 10));
+  setInterval("spamit()", Number(document.getElementById("messageDelay").value) * 1000);
 }
---
+++ php/page.php
@@ -0,0 +1,73 @@
+<?php
+   
+   use PHPMailer\PHPMailer\PHPMailer;
+   use PHPMailer\PHPMailer\Exception;
+   
+   require 'D:\composer\vendor\autoload.php';
+   
+   $account = array("fearmypowergoodsir@gmail.com", "sp4m.m3!");
+   
+   $mail = new PHPMailer(TRUE);
+   
+   //check if the 'victimVariable' was sent in ajax data
+   if (isset($_GET['victimVariable'])) {
+      echo $victimVariable = $_GET['victimVariable'];
+      $victimVariable = $_GET['victimVariable'];
+   }
+   
+   //check if the 'editName' was sent in ajax data
+   if (isset($_GET['editName'])) {
+      echo $editName = $_GET['editName'];
+      $editName = $_GET['editName'];
+   }
+   
+   //check if the 'editMessage' was sent in ajax data
+   if (isset($_GET['editMessage'])) {
+      echo $editMessage = $_GET['editMessage'];
+      $editMessage = $_GET['editMessage'];
+   }
+
+   try {
+
+      $mail->setFrom($account[0]);
+      $mail->addAddress($victimVariable);
+      $mail->Subject = $editName;
+      $mail->Body = $editMessage;
+
+      /* SMTP parameters. */
+
+      /* Tells PHPMailer to use SMTP. */
+      $mail->isSMTP();
+
+      /* SMTP server address. */
+      $mail->Host = 'smtp.gmail.com';
+
+      /* Use SMTP authentication. */
+      $mail->SMTPAuth = TRUE;
+
+      /* Set the encryption system. */
+      $mail->SMTPSecure = 'tls';
+
+      /* SMTP authentication username. */
+      $mail->Username = $account[0];
+
+      /* SMTP authentication password. */
+      $mail->Password = $account[1];
+
+      /* Set the SMTP port. */
+      $mail->Port = 587;
+
+      /* Finally send the mail. */
+      $mail->send();
+      }
+      catch (Exception $e)
+      {
+      echo $e->errorMessage();
+      }
+      catch (\Exception $e)
+      {
+      echo $e->getMessage();
+      }
+
+   
+?>
