a04fb5ecf8eb286d7143ed89ae027d90c87a717e
Ryan Aiello <thefloppydriver@gmail.com>
2019-08-25 16:09:41 -0400

    Update email.php

--- php/email.php
+++ php/email.php
@@ -1,4 +1,7 @@
 <?php header('Access-Control-Allow-Origin:https://thefloppydriver.github.io');
+header('Access-Control-Allow-Credentials:true');
+header('Access-Control-Allow-Methods:GET,HEAD,OPTIONS,POST,PUT');
+header('Access-Control-Allow-Headers:Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
    
    use PHPMailer\PHPMailer\PHPMailer;
    use PHPMailer\PHPMailer\Exception;
