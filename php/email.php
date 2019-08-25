0b64182bcce3ec37f0d9f8b72275c706e2da9bf5
Ryan Aiello <thefloppydriver@gmail.com>
2019-08-25 15:12:48 -0400

    Update email.php

--- php/email.php
+++ php/email.php
@@ -1,4 +1,4 @@
-<?php header('Access-Control-Allow-Origin:*');
+<?php header('Access-Control-Allow-Origin:github.io');
    
    use PHPMailer\PHPMailer\PHPMailer;
    use PHPMailer\PHPMailer\Exception;
